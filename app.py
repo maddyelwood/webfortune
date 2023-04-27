ff3b36a80b3fc722f312f4fe67f76a0bbd830ce5
Madalyn Elwood <maddy.elwood@icloud.com>
2023-04-27 10:47:21 -0500

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+import subprocess
+import cowsay
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return redirect(url_for('fortune'))
+
+@app.route('/fortune/', methods=['GET'])
+def fortune():
+    fortune = subprocess.check_output("fortune", shell=True)
+    return '<pre>' + fortune.decode("utf-8") + '</pre>'
+
+@app.route('/cowsay/<message>/', methods=['GET'])
+def cowsay(message):
+    return cowsay.cow(message)
+
+@app.route('/cowfortune/', methods=['GET'])
+def pipe_fortune():
+    pass
 
